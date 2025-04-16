deb79a438dfc3f87812c3b0c5b75800832161978
natwall17 <natwall17@gmail.com>
2025-04-16 10:12:19 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    //autoplay: {
+    // delay: 4500,
+    //  disableOnInteraction: false,
+    //},
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
